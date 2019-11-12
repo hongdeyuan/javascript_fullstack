4e40962f9249fe658d3ef10200d2f9667d2459ff
hongdeyuan <1737407069@qq.com>
2019-11-13 00:03:49 +0800

    fix(ME):修复wechat-tencentVideoApp-后台数据部分

--- Me/wechatapp/tencentVideoApp/miniprogram/data/entities.js
+++ Me/wechatapp/tencentVideoApp/miniprogram/data/entities.js
@@ -1,72 +1,72 @@
 module.exports = [
   {
     id: 5,
-    header: '演员',
+    header: '演员请就位',
     original_header: 'Welcome Winter in Colorado',
     description: "【演员】《情深深雨濛濛》重现依萍书桓 赵薇动容",
-    original_description: "Explore the Colorado mountains in the Range Rover Evoque Convertible with photographers Niki Csányi, Grace Ban and Neil Britto.",
+    original_description: "《演员请就位》是腾讯视频打造的国内首档导演选角真人秀，节目以选角为切口，以全影视化拍摄为呈现形式，旨在完整呈现影视行业从选角到成片的工业全貌，发现和挖掘对影视表演事业有坚定信念，致力于创作优质作品的优秀演员。陈凯歌、李少红、赵薇、郭敬明四位导演，将通过对年轻演员的甄别、激发、塑造，在节目中展开实力较量。",
     video: 'https://resources.ninghao.net/landrover/fall-field-guide-480.mp4',
     image: 'http://puui.qpic.cn/tv/0/329466791_1080607/0',
     duration_raw: 79,
     duration: '01:19',
     score: 8.5,
     universe: 36,
-    type: '古装',
+    type: '综艺',
     stars: [
       {
         id: 1,
-        starImg: 'https://puui.qpic.cn/media_img/0/1511121556439082/0',
-        name: '方逸伦'
+        starImg: 'https://puui.qpic.cn/media_img/0/797951571121949/0',
+        name: '陈凯歌'
       },
       {
         id: 2,
-        starImg: 'https://puui.qpic.cn/media_img/0/82643671569738548/0',
-        name: '凌美仕'
+        starImg: 'https://puui.qpic.cn/media_img/0/1063621572232237/0',
+        name: '李少红'
       },
       {
         id: 3,
-        starImg: 'https://puui.qpic.cn/media_img/0/1666021571025889/0',
-        name: '张博涵'
+        starImg: 'https://puui.qpic.cn/vstar_pic/0/name_78911_688t1501636611.jpg/0',
+        name: '赵薇'
       },
       {
         id: 4,
-        starImg: 'https://puui.qpic.cn/media_img/0/14833141555642878/0',
-        name: '周紫馨'
+        starImg: 'https://puui.qpic.cn/media_img/0/793141566542001/0',
+        name: '郭敬明'
       },
       {
         id: 5,
-        starImg: 'https://puui.qpic.cn/media_img/0/16538761571105031/0',
-        name: '董岩磊'
+        starImg: 'https://puui.qpic.cn/media_img/0/764211537263227/0',
+        name: '沙溢'
       },
       {
         id: 6,
-        starImg: 'https://puui.qpic.cn/media_img/0/82496601571025321/0',
-        name: '倪寒尽'
+        starImg: 'https://puui.qpic.cn/media_img/0/949211570670324/0',
+        name: '包文婧'
       },
       {
         id: 7,
-        starImg: 'https://puui.qpic.cn/media_img/0/82167041571105312/0',
-        name: '廖慧佳'
+        starImg: 'https://puui.qpic.cn/media_img/0/799311570670813/0',
+        name: '陈翔'
       },
       {
         id: 8,
-        starImg: 'https://puui.qpic.cn/media_img/0/12895381571032798/0',
-        name: '李金阳'
+        starImg: 'https://puui.qpic.cn/media_img/0/15729711570670828/0',
+        name: '陈小纭'
       },
       {
         id: 9,
-        starImg: 'https://puui.qpic.cn/media_img/0/null1513843570/0',
-        name: '廖彦龙'
+        starImg: 'https://puui.qpic.cn/media_img/0/15455261570670863/0',
+        name: '董力'
       },
       {
         id: 10,
-        starImg: 'https://puui.qpic.cn/media_img/0/13352041571105791/0',
-        name: '关畅'
+        starImg: 'https://puui.qpic.cn/media_img/0/82523101570671249/0',
+        name: '康可人'
       },
       {
         id: 11,
-        starImg: 'https://puui.qpic.cn/vstar_pic/0/name_1570471_688t1500031600.jpg/0',
-        name: '王浩歌'
+        starImg: 'https://puui.qpic.cn/media_img/0/15723841570671067/0',
+        name: '金靖'
       }
     ]
   },
@@ -75,69 +75,49 @@ module.exports = [
     header: '满满喜欢你',
     original_header: 'The Sound of Los Angeles',
     description: "【满满喜欢你】左岸展越联手对抗沈晨阳",
-    original_description: "Music producer, , turns up the volume on his latest tracks as he traverses the sprawling landscape of Los Angeles in search of fresh sounds to influence his music.",
+    original_description: "高三毕业在即，顾小满为了能与男神左岸继续做同学，拿出练习散打的精神死磕学习。自身刻苦努力加上天才学霸左岸的独家指导，顾小满达成最强逆袭，成功与男神考入同一所大学，向着爱情和学业一路高歌猛进。如果说高中生活两人之间是暗戳戳的小互动不间断，狠狠撩拨观众的少女心。那么大学生活则是依旧笑料不断，两人敞开发糖，甜度升级翻倍。都说最甜不过双向暗恋，两个初心萌动的人在靠近彼此的路上最是甜蜜有爱、百看不倦，甚至连途中的“意外”事件都显得萌趣十足",
     video: 'https://resources.ninghao.net/landrover/fall-field-guide-480.mp4',
     image: 'https://puui.qpic.cn/vcover_hz_pic/0/mzc0020059t7f7t1570794903/0',
     duration_raw: 94,
     duration: '01:34',
-    score: 8.5,
-    universe: 36,
-    type: '古装',
+    score: 7.6,
+    universe: 32,
+    type: '现代',
     stars: [
       {
         id: 1,
-        starImg: 'https://puui.qpic.cn/media_img/0/1511121556439082/0',
-        name: '方逸伦'
+        starImg: 'https://puui.qpic.cn/media_img/0/81900591529390711/0',
+        name: '鲁照华'
       },
       {
         id: 2,
-        starImg: 'https://puui.qpic.cn/media_img/0/82643671569738548/0',
-        name: '凌美仕'
+        starImg: 'https://puui.qpic.cn/media_img/0/16472021571902751/0',
+        name: '刘昱晗'
       },
       {
         id: 3,
-        starImg: 'https://puui.qpic.cn/media_img/0/1666021571025889/0',
-        name: '张博涵'
+        starImg: 'https://puui.qpic.cn/media_img/0/15970771570606858/0',
+        name: '谢治勋'
       },
       {
         id: 4,
-        starImg: 'https://puui.qpic.cn/media_img/0/14833141555642878/0',
-        name: '周紫馨'
+        starImg: 'https://puui.qpic.cn/media_img/0/14784021570761802/0',
+        name: '韩昕妤'
       },
       {
         id: 5,
-        starImg: 'https://puui.qpic.cn/media_img/0/16538761571105031/0',
-        name: '董岩磊'
+        starImg: 'https://puui.qpic.cn/media_img/0/15808571553060243/0',
+        name: '刘胤君'
       },
       {
         id: 6,
-        starImg: 'https://puui.qpic.cn/media_img/0/82496601571025321/0',
-        name: '倪寒尽'
+        starImg: 'https://puui.qpic.cn/media_img/0/82652631570604121/0',
+        name: '孙琰清'
       },
       {
         id: 7,
-        starImg: 'https://puui.qpic.cn/media_img/0/82167041571105312/0',
-        name: '廖慧佳'
-      },
-      {
-        id: 8,
-        starImg: 'https://puui.qpic.cn/media_img/0/12895381571032798/0',
-        name: '李金阳'
-      },
-      {
-        id: 9,
-        starImg: 'https://puui.qpic.cn/media_img/0/null1513843570/0',
-        name: '廖彦龙'
-      },
-      {
-        id: 10,
-        starImg: 'https://puui.qpic.cn/media_img/0/13352041571105791/0',
-        name: '关畅'
-      },
-      {
-        id: 11,
-        starImg: 'https://puui.qpic.cn/vstar_pic/0/name_1570471_688t1500031600.jpg/0',
-        name: '王浩歌'
+        starImg: 'https://puui.qpic.cn/media_img/0/null1508307753/0',
+        name: '宁文彤'
       }
     ]
   },
@@ -146,69 +126,39 @@ module.exports = [
     header: '机器人男友',
     original_header: 'The Color Of New Orleans',
     description: "【机器人男友·新剧首播】毛晓彤追爱高冷姜潮",
-    original_description: "Inspiration is everywhere when driving the vivid, technicolor streets of New Orleans. Artists Cleo Wade and Liza Voloshin visit the local art scene, drawing creativity from the city that has been their muse.",
+    original_description: "因一场误会相识，实习记者蒋梦言对实习医生林墨白一见钟情，开始主动出击，追求墨白。墨白在冰川探险时遭遇意外受伤，经科学家父亲林斯坦的改造而具有了一部分人工智能。梦言和墨白之间，开始了一段分分合合的坎坷爱情故事。与此同时，海外归来的神经科医生胡雨菲、珠宝设计师乔可铭，也加入了这场爱情攻坚战。四个人之间，上演虐心唯美的情感纠葛。事业上，梦言一步一步成长为资深新闻记者，墨白也渐渐成为更专业优秀的年轻医生。最终，墨白和梦言确认彼此的情感，选择相爱相守，不离不弃。同时，墨白与父亲林斯坦之间，父子恩怨消弭，亲情得以回归。",
     video: 'https://resources.ninghao.net/landrover/fall-field-guide-480.mp4',
     image: 'http://puui.qpic.cn/tv/0/333518185_1080607/0',
     duration_raw: 93,
     duration: '01:33',
-    score: 8.5,
-    universe: 36,
-    type: '古装',
+    score: 7.6,
+    universe: 42,
+    type: '现代',
     stars: [
       {
         id: 1,
-        starImg: 'https://puui.qpic.cn/media_img/0/1511121556439082/0',
-        name: '方逸伦'
+        starImg: 'https://i.gtimg.cn/qqlive/images/namelib/v688/7/6/3/93763.jpg',
+        name: '曾丽珍'
       },
       {
         id: 2,
-        starImg: 'https://puui.qpic.cn/media_img/0/82643671569738548/0',
-        name: '凌美仕'
+        starImg: 'https://puui.qpic.cn/media_img/0/null1517471486/0',
+        name: '姜潮'
       },
       {
         id: 3,
-        starImg: 'https://puui.qpic.cn/media_img/0/1666021571025889/0',
-        name: '张博涵'
+        starImg: 'https://puui.qpic.cn/media_img/0/null1508826300/0',
+        name: '毛晓彤'
       },
       {
         id: 4,
-        starImg: 'https://puui.qpic.cn/media_img/0/14833141555642878/0',
-        name: '周紫馨'
+        starImg: 'https://puui.qpic.cn/media_img/0/15765581530515109/0',
+        name: '孟子荻'
       },
       {
         id: 5,
-        starImg: 'https://puui.qpic.cn/media_img/0/16538761571105031/0',
-        name: '董岩磊'
-      },
-      {
-        id: 6,
-        starImg: 'https://puui.qpic.cn/media_img/0/82496601571025321/0',
-        name: '倪寒尽'
-      },
-      {
-        id: 7,
-        starImg: 'https://puui.qpic.cn/media_img/0/82167041571105312/0',
-        name: '廖慧佳'
-      },
-      {
-        id: 8,
-        starImg: 'https://puui.qpic.cn/media_img/0/12895381571032798/0',
-        name: '李金阳'
-      },
-      {
-        id: 9,
-        starImg: 'https://puui.qpic.cn/media_img/0/null1513843570/0',
-        name: '廖彦龙'
-      },
-      {
-        id: 10,
-        starImg: 'https://puui.qpic.cn/media_img/0/13352041571105791/0',
-        name: '关畅'
-      },
-      {
-        id: 11,
-        starImg: 'https://puui.qpic.cn/vstar_pic/0/name_1570471_688t1500031600.jpg/0',
-        name: '王浩歌'
+        starImg: 'https://puui.qpic.cn/media_img/0/null1510902669/0',
+        name: '王璟彦'
       }
     ],
   },
@@ -288,69 +238,29 @@ module.exports = [
     header: '哪吒之魔童降世',
     original_header: 'Norse Power',
     description: "【哪吒之魔童降世】吒儿来了！我命由我不由天！",
-    original_description: "Perfect for the Faroes' challenging terrain and twisting roads, Discovery Sport visits the remote islands where one restaurant kick started a fine food revolution that changed people's lives.",
+    original_description: "天地灵气孕育出一颗能量巨大的混元珠，元始天尊将混元珠提炼成灵珠和魔丸，灵珠投胎为人，助周伐纣时可堪大用；而魔丸则会诞出魔王，为祸人间。元始天尊启动了天劫咒语，3年后天雷将会降临，摧毁魔丸。太乙受命将灵珠托生于陈塘关李靖家的儿子哪吒身上。然而阴差阳错，灵珠和魔丸竟然被掉包。本应是灵珠英雄的哪吒却成了混世大魔王。调皮捣蛋顽劣不堪的哪吒却徒有一颗做英雄的心。然而面对众人对魔丸的误解和即将来临的天雷的降临，哪吒是否命中注定会立地成魔？他将何去何从？",
     video: 'https://resources.ninghao.net/landrover/fall-field-guide-480.mp4',
     image: 'https://puui.qpic.cn/vcover_hz_pic/0/zr5a67l333ehzu91570759226/0',
     duration_raw: 148,
     duration: '05:28',
-    score: 8.5,
-    universe: 36,
-    type: '古装',
+    score: 9.4,
+    universe: 1,
+    type: '动漫',
     stars: [
       {
         id: 1,
-        starImg: 'https://puui.qpic.cn/media_img/0/1511121556439082/0',
-        name: '方逸伦'
+        starImg: 'https://puui.qpic.cn/media_img/0/1073251563436142/0',
+        name: '饺子'
       },
       {
         id: 2,
-        starImg: 'https://puui.qpic.cn/media_img/0/82643671569738548/0',
-        name: '凌美仕'
+        starImg: 'https://puui.qpic.cn/media_img/0/82548831570843405/0',
+        name: '瀚墨'
       },
       {
         id: 3,
-        starImg: 'https://puui.qpic.cn/media_img/0/1666021571025889/0',
-        name: '张博涵'
-      },
-      {
-        id: 4,
-        starImg: 'https://puui.qpic.cn/media_img/0/14833141555642878/0',
-        name: '周紫馨'
-      },
-      {
-        id: 5,
-        starImg: 'https://puui.qpic.cn/media_img/0/16538761571105031/0',
-        name: '董岩磊'
-      },
-      {
-        id: 6,
-        starImg: 'https://puui.qpic.cn/media_img/0/82496601571025321/0',
-        name: '倪寒尽'
-      },
-      {
-        id: 7,
-        starImg: 'https://puui.qpic.cn/media_img/0/82167041571105312/0',
-        name: '廖慧佳'
-      },
-      {
-        id: 8,
-        starImg: 'https://puui.qpic.cn/media_img/0/12895381571032798/0',
-        name: '李金阳'
-      },
-      {
-        id: 9,
-        starImg: 'https://puui.qpic.cn/media_img/0/null1513843570/0',
-        name: '廖彦龙'
-      },
-      {
-        id: 10,
-        starImg: 'https://puui.qpic.cn/media_img/0/13352041571105791/0',
-        name: '关畅'
-      },
-      {
-        id: 11,
-        starImg: 'https://puui.qpic.cn/vstar_pic/0/name_1570471_688t1500031600.jpg/0',
-        name: '王浩歌'
+        starImg: 'https://puui.qpic.cn/media_img/0/82658751571122226/0',
+        name: '张珈铭'
       }
     ],
   },
