eeb813aa961b8537f45fd7a5854f4565a175dc70
hongdeyuan <1737407069@qq.com>
2020-01-09 10:20:32 +0800

    fix

---
+++ react-base/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// 浏览器专用的 dom
+ReactDOM.render(<App />, document.getElementById('root'));
+// react-native
+// ReactNative.render()
+// 核心的包：react
+// 平台相关的包：react-dom / react-native / react-TV
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
