0a021481ed35f6e175858b27e25605aeb80b5849
hongdeyuan <1737407069@qq.com>
2020-02-12 17:37:48 +0800

    feat(WoNiu):mpvue-新增商品详情购物

---
+++ WoNiu/mpvue/mpvue-koa/mpvue-shop-node/controllers/order/index.js
@@ -0,0 +1,81 @@
+const { mysql } = require('../../mysql')
+
+async function submitAction (ctx) {
+  const { openId } = ctx.request.body
+  let goodsId = ctx.request.body.goodsId
+  let allPrice = ctx.request.body.allPrice
+  // 是否存在订单
+  const isOrder = await mysql('nideshop_order').where({
+    'user_id': openId
+  }).select()
+  if (isOrder.length > 0) {
+    const data = await mysql('nideshop_order').where({
+      'user_id': openId
+    }).update({
+      user_id: openId,
+      goods_id: goodsId,
+      allprice: allPrice
+    })
+    if (data) {
+      ctx.body = {
+        data: true
+      }
+    } else {
+      ctx.body = {
+        data: false
+      }
+    }
+  } else {
+    const data = await mysql('nideshop_order').insert({
+      user_id: openId,
+      goods_id: goodsId,
+      allprice: allPrice
+    })
+    if (data) {
+      ctx.body = {
+        data: true
+      }
+    } else {
+      ctx.body = {
+        data: false
+      }
+    }
+  }
+}
+
+async function detailAction (ctx) {
+  const openId = ctx.query.openId
+  const addressId = ctx.query.addressId || ''
+  const orderDetail = await mysql('nideshop_order').where({
+    'user_id': openId
+  }).select()
+  
+  var goodsIds = orderDetail[0].goods_id.split(',')
+
+  const list = await mysql('nideshop_cart').andWhere({
+    'user_id': openId
+  }).whereIn('goods_id', goodsIds).select()
+  // 收货地址
+  var addressList;
+  if (addressId) {
+    addressList = await mysql('nideshop_address').where({
+      'user_id': openId,
+      'id': addressId
+    }).orderBy('is_default', 'desc').select()
+  } else {
+    addressList = await mysql('nideshop_address').where({
+      'user_id': openId
+    }).orderBy('is_default', 'desc').select()
+  }
+  
+  ctx.body = {
+    price: orderDetail[0].allprice,
+    goodsList: list,
+    address: addressList[0] || {}
+  }
+}
+
+module.exports = {
+  submitAction,
+  detailAction
+}
--- WoNiu/mpvue/mpvue-koa/mpvue-shop-node/routes/index.js
+++ WoNiu/mpvue/mpvue-koa/mpvue-shop-node/routes/index.js
@@ -16,4 +16,22 @@ router.get('/search/helperaction', controllers.search.index.helperAction) // 搜
 // 商品详情
 router.get('/goods/detailaction', controllers.goods.index.detailAction)
 
+
+// 收藏相关的接口
+router.post('/collect/addcollect', controllers.collect.index.addCollect)
+
+
+// 订单相关的接口
+router.post('/order/submitAction', controllers.order.index.submitAction)
+router.get('/order/detailAction', controllers.order.index.detailAction)
+
+
+// 购物车相关的接口
+router.post('cart/addCart', controllers.cart.index.addCart)
+
+
+// 收货地址相关的接口
+router.get('/address/getListAction', controllers.address.index.getListAction)
+router.get('/address/detailAction', controllers.address.index.detailAction)
+
 module.exports = router
