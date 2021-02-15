0f92f1666e2682322016fcb8ac647af8e7086b31
dehong <dehong@xsky.com>
2021-02-15 08:48:59 +0800

    add the egg learn
    Signed-off-by: dehong <dehong@xsky.com>

--- Me/electron/ninghao-electron/main.js
+++ Me/electron/ninghao-electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path')
 // electron 内置了window
 // 让 electron 支持热更新
@@ -23,6 +23,30 @@ const createWindow = () => {
       nodeIntegration: true
     }
   });
+  // 打开开发者工具，默认不打开
+  win.webContents.openDevTools()
   win.loadURL("https://www.meituan.com/")
+  // 关闭window时触发下列事件.
+  win.on('closed', function () {
+    win = null
+  });
 }
+// 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
 app.on('ready', createWindow)
+
+// 所有窗口关闭时退出应用.
+app.on('window-all-closed', function () {
+  // macOS中除非用户按下 `Cmd + Q` 显式退出,否则应用与菜单栏始终处于活动状态.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS中点击Dock图标时没有已打开的其余应用窗口时,则通常在应用中重建一个窗口
+  if (win === null) {
+    createWindow()
+  }
+})
+
+ // 你可以在这个脚本中续写或者使用require引入独立的js文件. 
